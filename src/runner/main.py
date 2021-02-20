bff59e44e819067b35ddff0953b2487295a46601
MilkSilk <42338877+MilkSilk@users.noreply.github.com>
2021-02-20 22:05:26 +0100

    adding a runner module

---
+++ src/runner/main.py
@@ -0,0 +1,29 @@
+import random
+import string
+
+from src.environment.environment import Environment
+from src.prisoner.prisoner import Prisoner
+from src.prisoner.learning_prisoner import LearningPrisoner
+
+
+def predefined_prisoners():
+    imprisoned_jacek = Prisoner(name="Jacek Jankowiak", decision_weights=[50, 50])
+    imprisoned_kasia = Prisoner(name="Kasia Górczyńska", decision_weights=[1, 0])
+    prison = Environment(imprisoned_jacek, imprisoned_kasia)
+
+    print(prison.pass_sentences())
+
+
+def q_learning_experiment(n_prisoners=100):
+    prisoners = []
+    for i in range(n_prisoners):
+        prisoner_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=3)) +\
+                      '-'.join(random.choices(string.ascii_uppercase + string.digits, k=3))
+        prisoners.append(LearningPrisoner("Prisoner "+prisoner_id+str(i)))
+    print([prisoner.name for prisoner in prisoners])
+    print(prisoners)
+
+
+if __name__ == "__main__":
+    # predefined_prisoners()
+    q_learning_experiment(100)
